eb6fbfd4628bd19da9ad3b8efc0bfb1857fc3321
Akshay Gupta <dev.akshaygupta@gmail.com>
2026-06-10 01:34:38 -0400

    create: implement concurrent ledger using single-writer channel pattern

---
+++ warm-ups/single-ledger-writer/single_writer_ledger.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+type Ledger struct {
+	balance int64
+}
+
+type Transaction struct {
+	Id     int8
+	amount int64
+}
+
+func submitTransaction(wg *sync.WaitGroup, trnsCh chan Transaction, trn Transaction) {
+	defer wg.Done()
+	trnsCh <- trn
+}
+
+func ledgerWorker(wg *sync.WaitGroup, trnsCh chan Transaction, ledgerBal *Ledger) {
+	defer wg.Done()
+	for trn := range trnsCh {
+		ledgerBal.balance += trn.amount
+		fmt.Println("TID", trn.Id, ", Transaction Amount -", trn.amount, ", Ledger Balance -", ledgerBal.balance)
+	}
+}
+
+func main() {
+	ledger := Ledger{}
+
+	trnsCh := make(chan Transaction)
+
+	var twg sync.WaitGroup
+	var lwg sync.WaitGroup
+
+	lwg.Add(1)
+	go ledgerWorker(&lwg, trnsCh, &ledger)
+	count := 0
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 3; j++ {
+			trns := Transaction{Id: int8(count), amount: rand.Int63n(491) + 10}
+			twg.Add(1)
+			go submitTransaction(&twg, trnsCh, trns)
+			count++
+		}
+	}
+	twg.Wait()
+	close(trnsCh)
+	lwg.Wait()
+
+	fmt.Println("\nFinal Ledger balance is: ", ledger.balance)
+}
